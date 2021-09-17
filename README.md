f1de5026e2d64bf9b9d7e5a99d5dc917799eaaf8
Teayeon-Lim <wingofyeon@naver.com>
2021-09-17 21:13:03 +0900

    feature/contextAPI로 리팩토링

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 ### Using lib: Styled-components, react-icons
 
-- create UI(Template)...
+- crea- create UI(Template)
+- use Context API 상태 관리
 
 <img src="https://user-images.githubusercontent.com/54977412/133735963-2186f12b-756b-4d0c-8744-0160a369f045.png" width="350" hegith="606"><img src="https://user-images.githubusercontent.com/54977412/133736046-e7c6b4b2-4768-45b0-9de9-aa58a7e0f3b1.png" width="350" hegith="606">
---
+++ src/Comonents/TodoContext.js
@@ -0,0 +1,85 @@
+import { createContext, useContext, useReducer, useRef } from 'react';
+
+const initialTodos = [
+  {
+    id: 1,
+    text: 'create project',
+    done: true,
+  },
+  {
+    id: 2,
+    text: 'components styling',
+    done: true,
+  },
+  {
+    id: 3,
+    text: 'create context ',
+    done: false,
+  },
+  {
+    id: 4,
+    text: '기능 구현',
+    done: false,
+  },
+];
+
+// CREATE, TOGGLE, REMOVE
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return state.concat(action.todo);
+    case 'TOGGLE':
+      return state.map(todo =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+      );
+    case 'REMOVE':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+// Context 최적화를 위한 분리(state, dispatch)
+const TodoStateContext = createContext();
+const TodoDispatchContext = createContext();
+const TodoNextIdContext = createContext();
+
+export function TodoProvider({ children }) {
+  const [state, dispatch] = useReducer(todoReducer, initialTodos);
+  const nextId = useRef(5);
+
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        <TodoNextIdContext.Provider value={nextId}>
+          {children}
+        </TodoNextIdContext.Provider>
+      </TodoDispatchContext.Provider>
+    </TodoStateContext.Provider>
+  );
+}
+
+// hooks
+export function useTodoState() {
+  const context = useContext(TodoStateContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+}
+
+export function useTodoDispatch() {
+  const context = useContext(TodoDispatchContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+}
+
+export function useTodoNextId() {
+  const context = useContext(TodoNextIdContext);
+  if (!context) {
+    throw new Error('Cannot find TodoProvider');
+  }
+  return context;
+}
